644a309ab56aef012ceb9c9284936a5f347694dc
Ayush <ayushgoyal0808@gmail.com>
2025-03-03 14:25:45 +0530

    [Ayush Goyal] ADD: Program to eligible for vote

---
+++ CanVote.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class CanVote{
+	public void static main(String[] args){
+		Scanner scn = new Scanner(System.in);
+		int[] ages = new int[10];
+		
+		for(int i=0;i<10;i++){
+			System.out.print("enter age of student : ")
+			ages[i] = scn.nextInt();
+		}
+		
+		for(int i=0;i<10;i++){
+			if(ages[i] >0){
+				System.out.println("Invalid age");
+			}
+			if(ages[i] >= 18){
+				System.out.println("The student with the age" + ages[i] + " can vote." );
+			}
+			else{
+				System.out.println("The student with the age" + ages[i] + " cannot vote.");
+			}
+			
+		}
+		
+		
+	}
+}
